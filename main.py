3cdbd5837d13624e648764a3917d914fc64695b5
WryOpussum <joshuaelm@jeditemple.com>
2022-04-05 11:29:28 -0500

    Fix ex-coop and add Render96 and Odyssey mario moveset.

--- main.py
+++ main.py
@@ -132,6 +132,66 @@ class MainWindow(Gtk.ApplicationWindow):
         build_page_2.connect('clicked', self.compileSm64excoop)
         launch_page_2.connect('clicked', self.launchSm64excoop)
         
+        #Render96
+        
+        box_page_3 = Gtk.Box.new(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        stack.add_titled(child=box_page_3, name='Render96', title='Render96')
+
+        build_page_3 = Adw.SplitButton(label="Start Compilation")
+        build_page_3.set_menu_model(menu)
+        build_page_3.set_halign(align=Gtk.Align.CENTER)
+        build_page_3.set_valign(align=Gtk.Align.START)
+        build_page_3.set_hexpand(expand=True)
+        build_page_3.set_vexpand(expand=True)
+        
+        launch_page_3 = Gtk.Button(label="Launch")
+        launch_page_3.set_halign(align=Gtk.Align.CENTER)
+        launch_page_3.set_valign(align=Gtk.Align.START)
+        launch_page_3.set_hexpand(expand=True)
+        launch_page_3.set_vexpand(expand=True)
+        
+        description_page_3 = Gtk.Label.new(str='Render96 is a fork of sm64ex with Dynos and models!')
+        description_page_3.set_halign(align=Gtk.Align.CENTER)
+        description_page_3.set_valign(align=Gtk.Align.END)
+        description_page_3.set_hexpand(expand=True)
+        description_page_3.set_vexpand(expand=True)
+        
+        box_page_3.append(child=description_page_3)
+        box_page_3.append(child=build_page_3)
+        box_page_3.append(child=launch_page_3)
+        build_page_3.connect('clicked', self.compileRender96)
+        launch_page_3.connect('clicked', self.launchRender96)
+        
+        #Moon64
+        
+        box_page_4 = Gtk.Box.new(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        stack.add_titled(child=box_page_4, name='SM64 Odyssey Mario Moveset', title='SM64 Odyssey Mario Moveset')
+
+        build_page_4 = Adw.SplitButton(label="Start Compilation")
+        build_page_4.set_menu_model(menu)
+        build_page_4.set_halign(align=Gtk.Align.CENTER)
+        build_page_4.set_valign(align=Gtk.Align.START)
+        build_page_4.set_hexpand(expand=True)
+        build_page_4.set_vexpand(expand=True)
+        
+        launch_page_4 = Gtk.Button(label="Launch")
+        launch_page_4.set_halign(align=Gtk.Align.CENTER)
+        launch_page_4.set_valign(align=Gtk.Align.START)
+        launch_page_4.set_hexpand(expand=True)
+        launch_page_4.set_vexpand(expand=True)
+        
+        description_page_4 = Gtk.Label.new(str='SM64 Odyssey Mario Moveset is Mario 64 with mario odyssey abilities!')
+        description_page_4.set_halign(align=Gtk.Align.CENTER)
+        description_page_4.set_valign(align=Gtk.Align.END)
+        description_page_4.set_hexpand(expand=True)
+        description_page_4.set_vexpand(expand=True)
+        
+        box_page_4.append(child=description_page_4)
+        box_page_4.append(child=build_page_4)
+        box_page_4.append(child=launch_page_4)
+        build_page_4.connect('clicked', self.compileSm64oom)
+        launch_page_4.connect('clicked', self.launchSm64oom)
+        
         
         
         # Sidebar
@@ -158,16 +218,35 @@ class MainWindow(Gtk.ApplicationWindow):
     def launchSm64excoop(self, menu):         
             
             os.system('cd sm64ex-coop/build/us_pc/ && ./sm64.us.f3dex2e')
-               
+            
+    def launchRender96(self, menu):         
+            
+            os.system('cd Render96ex/build/us_pc/ && ./sm64.us.f3dex2e')
+            
+    def launchSm64oom(self, menu):         
+            
+            os.system('cd sm64pc-omm/build/us_pc/ && ./sm64.us.f3dex2e')
+                          
     def compileSm64ex(self, menu):
             
             print(compilationSpeed)
             os.system('git clone https://github.com/sm64pc/sm64ex.git && cp -r baserom.us.z64 sm64ex && cd sm64ex && make' + compilationSpeed) 
+            
+    def compileSm64oom(self, menu):
+            
+            print(compilationSpeed)
+            os.system('git clone https://github.com/sm64pc/sm64ex.git sm64omm && git clone https://github.com/PeachyPeachSM64/sm64pc-omm.git && mv sm64pc-omm/patch/omm.patch sm64omm/enhancements && rm -rf sm64pc-omm && cd sm64omm && git apply enhancements/omm.patch && make' + compilationSpeed) 
+            
+            
+    def compileRender96(self, menu):
+            
+            print(compilationSpeed)
+            os.system('git clone https://github.com/Render96/Render96ex.git --branch alpha && cp -r baserom.us.z64 Render96ex && cd Render96ex && make' + compilationSpeed) 
                   
     def compileSm64excoop(self, menu):
       
             print(compilationSpeed)
-            os.system('git clone https://github.com/djoslin0/sm64ex-coop.git && cp -r baserom.us.z64 sm64ex && cd sm64ex-coop && make' + compilationSpeed) 
+            os.system('git clone https://github.com/djoslin0/sm64ex-coop.git && cp -r baserom.us.z64 sm64ex-coop && cd sm64ex-coop && make' + compilationSpeed) 
             
     def Fastest(self, action, param):
         global compilationSpeed
