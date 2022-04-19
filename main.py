61ab6722aa23239235befa41eb3c1d658b6b9aa7
WryOpussum <joshuaelm@jeditemple.com>
2022-04-19 09:59:59 -0500

    removed omm and added sm64 plus aswell as desktop notifications

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python
+
 import sys
 import gi
 import os
 import logging
+import asyncio
+import threading
+gi.require_version('Notify', '0.7')
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw, Gio
+from gi.repository import Notify, Gtk, Adw, Gio
 
 
 class MainWindow(Gtk.ApplicationWindow):
@@ -19,13 +24,41 @@ class MainWindow(Gtk.ApplicationWindow):
         
         self.box1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         self.set_child(self.box1)
+
+
+        # setup compile functions
         
-       
+        def compileSm64ex(self, widget):
+
+            print(compilationSpeed)
+            os.system('git clone https://github.com/sm64pc/sm64ex.git && cp -r baserom.us.z64 sm64ex && cd sm64ex && make' + compilationSpeed)
+
+
+            n.show()
+
+
+
+
+
+
+
+
+        #notification setup
+
+
+        Notify.init("sm64LinuxBuilder")
+        global n
+        n = Notify.Notification.new("sm64LinuxBuilder", "Build Complete!")
 
           
         
         #flap//pane
         
+        flap_Toggle_button = Gtk.ToggleButton.new()
+        flap_Toggle_button.set_icon_name(icon_name='sidebar-show-symbolic')
+        flap_Toggle_button.connect('clicked', self.on_flap_button_toggled)
+        self.header.pack_start(child=flap_Toggle_button)
+
         self.adw_flap = Adw.Flap.new()
         self.adw_flap.set_reveal_flap(reveal_flap=True)
         self.adw_flap.set_locked(locked=True)
@@ -162,10 +195,11 @@ class MainWindow(Gtk.ApplicationWindow):
         build_page_3.connect('clicked', self.compileRender96)
         launch_page_3.connect('clicked', self.launchRender96)
         
-        #Moon64
+        #sm64 plus
         
         box_page_4 = Gtk.Box.new(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        stack.add_titled(child=box_page_4, name='SM64 Odyssey Mario Moveset', title='SM64 Odyssey Mario Moveset')
+        stack.add_titled(child=box_page_4, name='sm64plus', title='sm64plus')
+
 
         build_page_4 = Adw.SplitButton(label="Start Compilation")
         build_page_4.set_menu_model(menu)
@@ -173,14 +207,13 @@ class MainWindow(Gtk.ApplicationWindow):
         build_page_4.set_valign(align=Gtk.Align.START)
         build_page_4.set_hexpand(expand=True)
         build_page_4.set_vexpand(expand=True)
-        
         launch_page_4 = Gtk.Button(label="Launch")
         launch_page_4.set_halign(align=Gtk.Align.CENTER)
         launch_page_4.set_valign(align=Gtk.Align.START)
         launch_page_4.set_hexpand(expand=True)
         launch_page_4.set_vexpand(expand=True)
         
-        description_page_4 = Gtk.Label.new(str='SM64 Odyssey Mario Moveset is Mario 64 with mario odyssey abilities!')
+        description_page_4 = Gtk.Label.new(str='Mario 64 with neat diffrences like extended movesets.')
         description_page_4.set_halign(align=Gtk.Align.CENTER)
         description_page_4.set_valign(align=Gtk.Align.END)
         description_page_4.set_hexpand(expand=True)
@@ -189,18 +222,22 @@ class MainWindow(Gtk.ApplicationWindow):
         box_page_4.append(child=description_page_4)
         box_page_4.append(child=build_page_4)
         box_page_4.append(child=launch_page_4)
-        build_page_4.connect('clicked', self.compileSm64oom)
-        launch_page_4.connect('clicked', self.launchSm64oom)
-        
-        
-        
+        build_page_4.connect('clicked', self.compileSm64plus)
+        launch_page_4.connect('clicked', self.launchSm64plus)
+
+
+
+
+
         # Sidebar
         
         stack_sidebar = Gtk.StackSidebar.new()
         stack_sidebar.set_stack(stack=stack)
         self.adw_flap.set_flap(flap=stack_sidebar)
         
-        
+    def on_flap_button_toggled(self, widget):
+        self.adw_flap.set_reveal_flap(not self.adw_flap.get_reveal_flap())
+
         #Headerbar menu
         
         #entries
@@ -211,43 +248,47 @@ class MainWindow(Gtk.ApplicationWindow):
     compilationSpeed = " -j2"
     
     
-    def launchSm64ex(self, menu):         
+    def launchSm64ex(self, widget, menu):
+
+            os.system('./sm64ex/build/us_pc/sm64.us.f3dex2e ')
+
+    def launchSm64plus(self, widget, menu):
             
-            os.system('./sm64ex/build/us_pc/sm64.us.f3dex2e')
+            os.system('./sm64plus/build/us_pc/sm64.us ')
             
     def launchSm64excoop(self, menu):         
             
-            os.system('cd sm64ex-coop/build/us_pc/ && ./sm64.us.f3dex2e')
+            os.system('cd sm64ex-coop/build/us_pc/ && ./sm64.us.f3dex2e ')
             
     def launchRender96(self, menu):         
             
-            os.system('cd Render96ex/build/us_pc/ && ./sm64.us.f3dex2e')
+            os.system('cd Render96ex/build/us_pc/ && ./sm64.us.f3dex2e ')
             
-    def launchSm64oom(self, menu):         
-            
-            os.system('cd sm64pc-omm/build/us_pc/ && ./sm64.us.f3dex2e')
-                          
     def compileSm64ex(self, menu):
             
             print(compilationSpeed)
-            os.system('git clone https://github.com/sm64pc/sm64ex.git && cp -r baserom.us.z64 sm64ex && cd sm64ex && make' + compilationSpeed) 
+            os.system('git clone https://github.com/sm64pc/sm64ex.git && cp -r baserom.us.z64 sm64ex && cd sm64ex && make' + compilationSpeed)
+            n.show()
             
-    def compileSm64oom(self, menu):
+    def compileSm64plus(self, menu):
             
             print(compilationSpeed)
-            os.system('git clone https://github.com/sm64pc/sm64ex.git sm64omm && git clone https://github.com/PeachyPeachSM64/sm64pc-omm.git && mv sm64pc-omm/patch/omm.patch sm64omm/enhancements && rm -rf sm64pc-omm && cd sm64omm && git apply enhancements/omm.patch && make' + compilationSpeed) 
-            
-            
+            os.system('git clone https://github.com/MorsGames/sm64plus.git && cp -r baserom.us.z64 sm64plus && cd sm64ex && make' + compilationSpeed)
+            n.show()
+
     def compileRender96(self, menu):
             
             print(compilationSpeed)
-            os.system('git clone https://github.com/Render96/Render96ex.git --branch alpha && cp -r baserom.us.z64 Render96ex && cd Render96ex && make' + compilationSpeed) 
-                  
+            os.system('git clone https://github.com/Render96/Render96ex.git --branch tester_rt64alpha && cp -r baserom.us.z64 Render96ex && cd Render96ex && make' + compilationSpeed)
+            n.show()
     def compileSm64excoop(self, menu):
       
             print(compilationSpeed)
-            os.system('git clone https://github.com/djoslin0/sm64ex-coop.git && cp -r baserom.us.z64 sm64ex-coop && cd sm64ex-coop && make' + compilationSpeed) 
-            
+            os.system('git clone https://github.com/djoslin0/sm64ex-coop.git && cp -r baserom.us.z64 sm64ex-coop && cd sm64ex-coop && make' + compilationSpeed)
+
+            n.show()
+
+
     def Fastest(self, action, param):
         global compilationSpeed
         compilationSpeed = " -j"
